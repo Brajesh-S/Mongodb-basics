6221ee1b1c5718e9729ab622c433bf736323dacc
brajesh-s <brajeshsekar@outlook.com>
2022-10-17 01:14:26 -0700

    schema

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')  
     .then(() => console.log('connected to MongoDB...'))
     .catch(err => console.error(' could not connect to MongoDB...', err))
+    
+const CourseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: {type: Date, default: Date.now },
+    isPublished: Boolean
+
+})
