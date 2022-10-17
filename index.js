2e8dd83deb8740c204e4aea18f116e64514f75ad
brajesh-s <brajeshsekar@outlook.com>
2022-10-17 01:30:57 -0700

    Creating models

--- index.js
+++ index.js
@@ -10,5 +10,11 @@ const CourseSchema = new mongoose.Schema({
     tags: [ String ],
     date: {type: Date, default: Date.now },
     isPublished: Boolean
-
-})
+});
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+    name: 'Node.js Course',
+    author: 'Aris', 
+    tags: ['node', 'backend'],
+    isPublished: true
+});
